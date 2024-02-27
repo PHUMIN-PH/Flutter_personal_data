6fe277b65e79a14477180217fb53a06f7a04a0fc
PHUMIN-PH <phuwis.dev@gmail.com>
2024-02-28 04:27:07 +0700

    update tabBar widget and generate card of users. add del btn and alt tas

---
+++ lib/views/activity.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class ActivityListView extends StatelessWidget {
+  final activityicon;
+  final passTap;
+  final String activityName;
+  final String activityDetail;
+  final String activityOnTap;
+  // final int numberOfActivityName;
+
+  const ActivityListView({
+    Key? key,
+    required this.activityicon,
+    required this.activityName,
+    required this.activityDetail,
+    required this.activityOnTap,
+    this.passTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: GestureDetector(
+          onTap: passTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                        padding: EdgeInsets.all(8),
+                        color: Colors.grey[200],
+                        child: Icon(
+                          activityicon,
+                          color: Colors.orange[800],
+                          size: 40,
+                        )),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        activityName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        // numberOfActivityName.toString() + 'PersonalCare Data from hospital',
+                        activityDetail,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // Icon(Icons.more_horiz),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SmartWatchListView extends StatelessWidget {
+  final color;
+  final colorBg;
+  final icon;
+  final SmartWatchActivityName;
+  final SmartWatchActivityDetail;
+
+  const SmartWatchListView({
+    Key? key,
+    required this.color,
+    required this.colorBg,
+    required this.icon,
+    required this.SmartWatchActivityName,
+    required this.SmartWatchActivityDetail,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: colorBg,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                    padding: EdgeInsets.all(16),
+                    color: color,
+                    child: Icon(
+                      icon,
+                      color: Colors.white,
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+                width: 120,
+              ),
+              Text(
+                SmartWatchActivityName,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                // numberOfActivityName.toString() + 'PersonalCare Data from hospital',
+                SmartWatchActivityDetail,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/views/personalData.dart
+++ lib/views/personalData.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:personal_data/views/activity.dart';
+import 'model/personalData_fetch.dart';
 
 class PersonalData extends StatefulWidget {
   const PersonalData({super.key});
@@ -7,9 +13,104 @@ class PersonalData extends StatefulWidget {
   State<PersonalData> createState() => _PersonalDataState();
 }
 
-class _PersonalDataState extends State<PersonalData> {
+class _PersonalDataState extends State<PersonalData>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Future<List<User>> fetchUser() async {
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/photos' as Uri);
+
+    String logResponse = response.statusCode.toString();
+
+    if (response.statusCode == 200) {
+      print('ResponseStatusCode: $logResponse'); // Check Status Code = 200
+      print('ResponseBody: ' + response.body); // Read Data in Array
+
+      List<dynamic> responseJson = json.decode(response.body);
+      return responseJson.map((m) => new User.fromJson(m)).toList();
+    } else {
+      throw Exception('error :(');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar_Method(),
+      body: Expanded(
+        child: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView(
+              padding: EdgeInsets.all(20),
+              children: [
+                Card(
+                  color: Colors.amberAccent,
+                  child: ListTile(
+                    title: Text("Jenny Lincon"),
+                    subtitle: Text("Java Developer  Tel: 8845985"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      iconSize: 50,
+                      color: Colors.redAccent,
+                      onPressed: () {
+                        onPressIconInsert();
+                      },
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.amberAccent,
+                  child: ListTile(
+                    title: Text("Jenny Lincon"),
+                    subtitle: Text("Java Developer  Tel: 8845985"),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Text("Fetch filter data here"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar AppBar_Method() {
+    return AppBar(
+      backgroundColor: Colors.amber,
+      title: const Text("Personal Data"),
+      centerTitle: true,
+      bottom: TabBar(
+        controller: _tabController,
+        tabs: const <Widget>[
+          Tab(
+            icon: Icon(Icons.person_2),
+          ),
+          Tab(
+            icon: Icon(Icons.filter_list),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onPressIconInsert() {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Alert snackbar')));
   }
 }
