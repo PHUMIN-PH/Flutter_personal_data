3dab7d517e2c5bb3bd9f554910947f3c1ec48292
PHUMIN-PH <phuwis.dev@gmail.com>
2024-02-29 00:57:31 +0700

    Success ID Card (Fix data)

--- lib/views/infoData.dart
+++ lib/views/infoData.dart
@@ -30,7 +30,7 @@ class _InfoDataState extends State<InfoData> {
         margin: EdgeInsets.all(15),
         padding: EdgeInsets.all(10),
         width: double.infinity,
-        height: 300,
+        height: 315,
         decoration: BoxDecoration(
           color: Colors.blue[100],
           borderRadius: BorderRadius.circular(16),
@@ -44,12 +44,13 @@ class _InfoDataState extends State<InfoData> {
                   'บัตรประจำตัวประชาชน ',
                   style: TextStyle(fontSize: 15),
                 ),
-                SizedBox(
-                  width: 5,
-                ),
+                SizedBox(width: 5),
                 Text(
                   'Thai National ID Card',
-                  style: TextStyle(fontSize: 17, color: TextColorEN,),
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: TextColorEN,
+                  ),
                 ),
               ],
             ),
@@ -65,9 +66,7 @@ class _InfoDataState extends State<InfoData> {
                     ),
                   ],
                 ),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Text(
                   personal_idCard,
                   style: TextStyle(
@@ -99,49 +98,37 @@ class _InfoDataState extends State<InfoData> {
                             Row(
                               children: [
                                 Text('ชื่อ นามสกุล'),
-                                SizedBox(
-                                  width: 17,
-                                ),
+                                SizedBox(width: 17),
                                 Text(
                                   personal_nameTH,
-                                  style: TextStyle(fontSize: 17),
+                                  style: centerStyleTH(),
                                 )
                               ],
                             ),
                             Row(
                               children: [
                                 Text("Full Name"),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                Text(
-                                  personal_nameEN,
-                                  style: TextStyle(fontSize: 15,
-                                  color: TextColorEN),
-                                ),
+                                SizedBox(width: 15),
+                                Text(personal_nameEN, style: centerStyleEN()),
                               ],
                             ),
                             Row(
                               children: [
                                 Text("เกิดวันที่"),
-                                SizedBox(
-                                  width: 17,
-                                ),
+                                SizedBox(width: 17),
                                 Text(
                                   personal_birthTH,
-                                  style: TextStyle(fontSize: 17),
+                                  style: centerStyleTH(),
                                 ),
                               ],
                             ),
                             Row(
                               children: [
                                 Text("Date of Birth"),
-                                SizedBox(
-                                  width: 15,
-                                ),
+                                SizedBox(width: 15),
                                 Text(
                                   personal_birthEN,
-                                  style: TextStyle(fontSize: 15,color: TextColorEN),
+                                  style: centerStyleEN(),
                                 ),
                               ],
                             ),
@@ -153,23 +140,58 @@ class _InfoDataState extends State<InfoData> {
                 ),
               ],
             ),
+            Text("Ad: " + personal_address),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: [
-                        Text("Ad: "),
-                        Text(personal_address),
-                      ],
+                    Text(
+                      personal_issueTH,
+                      style: footerCardStyleTH(),
                     ),
-                    Text(personal_issueTH),
-                    Text("วันออกบัตร"),
-                    
+                    Text(
+                      "วันออกบัตร",
+                      style: footerCardStyleTH(),
+                    ),
+                    Text(
+                      personal_issueEN,
+                      style: footerCardStyleEN(),
+                    ),
+                    Text(
+                      "Date of issue",
+                      style: footerCardStyleEN(),
+                    )
                   ],
                 ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      personal_expiryTH,
+                      style: footerCardStyleTH(),
+                    ),
+                    Text(
+                      "วันออกบัตร",
+                      style: footerCardStyleTH(),
+                    ),
+                    Text(
+                      personal_expiryEN,
+                      style: footerCardStyleEN(),
+                    ),
+                    Text(
+                      "Date of issue",
+                      style: footerCardStyleEN(),
+                    )
+                  ],
+                ),
+                Icon(
+                  Icons.person_outlined,
+                  size: 100,
+                )
               ],
             ),
           ],
@@ -178,5 +200,12 @@ class _InfoDataState extends State<InfoData> {
     );
   }
 
+  TextStyle centerStyleEN() => TextStyle(fontSize: 15, color: TextColorEN);
+
+  TextStyle centerStyleTH() =>
+      TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  TextStyle footerCardStyleTH() => TextStyle(fontSize: 12);
+  TextStyle footerCardStyleEN() => TextStyle(fontSize: 12, color: TextColorEN);
   MaterialColor get TextColorEN => Colors.blue;
 }
